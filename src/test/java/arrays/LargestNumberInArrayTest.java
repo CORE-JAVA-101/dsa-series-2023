665cb1440822e9be4710078a7decb09a355aec53
rkpatra201 <rabindrapatra825@gmail.com>
2023-04-30 23:24:23 +0530

    format code

--- src/test/java/arrays/LargestNumberInArrayTest.java
+++ src/test/java/arrays/LargestNumberInArrayTest.java
@@ -8,7 +8,7 @@ public class LargestNumberInArrayTest {
 
     @Test
     public void solution() {
-        int arr[] = {1,5,3,7, 8, 9, 12, 0, 9, 8};
+        int arr[] = {1, 5, 3, 7, 8, 9, 12, 0, 9, 8};
         LargestNumberInArray largestNumberInArray = new LargestNumberInArray();
         int actualValue = largestNumberInArray.solution(arr);
         Assert.assertEquals(12, actualValue);
